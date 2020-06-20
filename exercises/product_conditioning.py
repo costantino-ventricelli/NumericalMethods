90fe55bf0afe5f975b1271d30b9237fafd69f28f
costantino-ventricelli <costantini.ventricelli@gmail.com>
2020-06-20 11:32:51 +0200

    fix product_conditioning.py

--- exercises/product_conditioning.py
+++ exercises/product_conditioning.py
@@ -1,3 +1,5 @@
+#   Come per il condizionamento della somma, quello del prodotto dipende dal problema stesso.
+
 import numpy as np
 
 
@@ -9,32 +11,32 @@ class ProductConditioning:
         self.__perturbation = perturbation
         self.__first_perturbed = self.__first_number + self.__perturbation
         self.__second_perturbed = self.__second_number + self.__perturbation
-        self.__perturbed_product = self.__first_perturbed * self.__second_perturbed
-        self.__normal_product = self.__first_number * self.__second_number
+        self.__perturbed_sum = self.__first_perturbed + self.__second_perturbed
+        self.__normal_sum = self.__first_number + self.__second_number
 
-        def get_absolute_error_product(self):
-            return np.abs(self.__perturbed_product - self.__normal_product)
+    def get_absolute_error_product(self):
+        return np.abs(self.__perturbed_sum - self.__normal_sum)
 
-        def get_relative_error_sum(self):
-            if self.__normal_sum != 0:
-                return np.abs(self.__perturbed_product - self.__normal_product) / np.abs(self.__perturbed_product)
-            else:
-                return 0
+    def get_relative_error_product(self):
+        if self.__normal_sum != 0:
+            return np.abs(self.__perturbed_sum - self.__normal_sum) / np.abs(self.__perturbed_sum)
+        else:
+            return 0
 
-        def get_first_number_absolute_error(self):
-            return np.abs(self.__first_perturbed - self.__first_number)
+    def get_first_number_absolute_error(self):
+        return np.abs(self.__first_perturbed - self.__first_number)
 
-        def get_first_number_relative_error(self):
-            if self.__first_perturbed != 0:
-                return np.abs(self.__first_perturbed - self.__first_number) / np.abs(self.__first_perturbed)
-            else:
-                return 0
+    def get_first_number_relative_error(self):
+        if self.__first_perturbed != 0:
+            return np.abs(self.__first_perturbed - self.__first_number) / np.abs(self.__first_perturbed)
+        else:
+            return 0
 
-        def get_second_number_absolute_error(self):
-            return np.abs(self.__second_perturbed - self.__second_number)
+    def get_second_number_absolute_error(self):
+        return np.abs(self.__second_perturbed - self.__second_number)
 
-        def get_second_number_relative_error(self):
-            if self.__second_perturbed != 0:
-                return np.abs(self.__second_perturbed - self.__second_number) / self.__second_perturbed
-            else:
-                return 0
+    def get_second_number_relative_error(self):
+        if self.__second_perturbed != 0:
+            return np.abs(self.__second_perturbed - self.__second_number) / self.__second_perturbed
+        else:
+            return 0
